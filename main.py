031125f7380a07ecfe1d8ef47aefa7e5011742d9
devlunch4 <devlunch4@gmail.com>
2021-09-26 21:24:25 +0900

    20210926 2130 copy_move() created, added

--- main.py
+++ main.py
@@ -8,12 +8,6 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-def copy_move():  # TODO:
-    import shutil
-    path = "/resource"
-    print()
-
-
 def make_list():
     import os
 
@@ -24,15 +18,28 @@ def make_list():
     list_file = open('./resource/list.txt', 'r').read().split('\n')
     print("$$$ list_file:", list_file)
 
-    new_list = []
+    new_copy_list = []
     for x in file_list:
         for y in list_file:
             if y in x:
                 if y is not None and y != "":
                     print("x", x)
                     print("y", y)
-                    new_list.append(x)
-    print("$$$ new_list: ", new_list)
+                    new_copy_list.append(x)
+    print("$$$ new_list: ", new_copy_list)
+    copy_move(new_copy_list)
+
+
+def copy_move(new_copy_list):
+    import shutil
+    i = 0
+    for file in new_copy_list:
+        src = './resource/' + file
+        print("$$$ file: ", file)
+        dst = "./resource/copy/"
+        shutil.copy2(src, dst)
+        i += 1
+        print("cnt copy_move i:", i)
 
 
 # Press the green button in the gutter to run the script.
